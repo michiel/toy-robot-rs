9830402aa94b929b3fb3b0c0d53ab30757163910
Michiel Kalkman <michiel@nosuchtype.com>
2017-03-30 20:44:53 +0200

    feature: Add command grammar and enums
     * Add rust-peg dependency for parser generation
     * Add build.rs for build-time generation of corresponding parser code
     * Add corresponding enums for Direction and Command
     * Add basic tests

---
+++ build.rs
@@ -0,0 +1,5 @@
+extern crate peg;
+
+fn main() {
+    peg::cargo_build("src/toyrobot_grammar.rustpeg");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+#[allow(dead_code)]
+mod toyrobot_grammar {
+    include!(concat!(env!("OUT_DIR"), "/toyrobot_grammar.rs"));
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Command {
+    Move,
+    Left,
+    Right,
+    Report,
+    Place(i64, i64, Direction),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum CommandParseError {
+    InvalidCommand(String),
+}
+
+pub fn read_command(s: &str) -> Result<Command, CommandParseError> {
+    match toyrobot_grammar::command(s) {
+        Ok(command) => Ok(command),
+        Err(_) => {
+            Err(CommandParseError::InvalidCommand(format!("ERROR :Invalid command : {:?}", s)))
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_read_basic_commands() {
+        assert_eq!(read_command(&"MOVE").unwrap(), Command::Move);
+        assert_eq!(read_command(&"LEFT").unwrap(), Command::Left);
+        assert_eq!(read_command(&"RIGHT").unwrap(), Command::Right);
+        assert_eq!(read_command(&"REPORT").unwrap(), Command::Report);
+    }
+
+    #[test]
+    fn can_read_place_command() {
+        let res1 = read_command(&"PLACE 1,2,WEST");
+        assert_eq!(res1.unwrap(), Command::Place(1, 2, Direction::West));
+
+        let res2 = read_command(&"PLACE 2,4,NORTH");
+        assert_eq!(res2.unwrap(), Command::Place(2, 4, Direction::North));
+
+        let res3 = read_command(&"PLACE X,Y,Z");
+        assert_eq!(res3.is_err(), true);
+    }
 }
